bb38c9a54801e744a7cc1338ab2824473fb4a943
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-06-24 15:04:30 -0300

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,12 +1,11 @@
 <?php
 $host = "localhost";
-$user = "root";         // cambiar si tu usuario es distinto
-$pass = "";             // poner tu contraseña de MySQL si tiene
+$user = "root";        
+$pass = "";            
 $dbname = "liquidacion_sueldos";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// Verifica errores
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
